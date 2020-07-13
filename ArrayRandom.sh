24baee448720233f8c78979f6a57d3106752e2e6
Rohini657 <rohinijadhav3196@gmail.com>
2020-07-13 11:07:31 +0530

    FirstProblem

---
+++ ArrayRandom.sh
@@ -0,0 +1,37 @@
+for i in {1..10}
+do
+num[$i]=$(( RANDOM % 899 + 100 ))
+done
+echo "${num[@]}"
+#second maximum number findout......
+first=${num[1]}
+second=${num[2]}
+for i in {1..10}
+do
+if [ ${num[$i]} -gt $first ]
+then
+second=$first
+first=${num[i]}
+fi
+if [ ${num[$i]} -gt $second -a ${num[$i]} -ne $first ]
+then
+second=${num[$i]}
+fi
+done
+echo "second maximum number is :"$second
+#second minimum number findout............
+first=${num[1]}
+second=${num[2]}
+for i in {1..10}
+do
+if [ ${num[$i]} -lt $first ]
+then
+second=$first
+first=${num[i]}
+fi
+if [ ${num[$i]} -lt $second -a ${num[$i]} -ne $first ]
+then
+second=${num[$i]}
+fi
+done
+echo "second minimum number is :"$second
